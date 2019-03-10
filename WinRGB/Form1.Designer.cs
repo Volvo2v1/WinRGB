40dfe47f34628c04721596ef4d6b82e470c049e3
Volvo2v1 <volvo2v1@gmail.com>
2019-03-10 18:04:42 +0100

    WinRGB 1.0
    Init Release

---
+++ WinRGB/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DeepPink;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Leelawadee UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(25, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Change Color";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 33);
+            this.textBox1.MaxLength = 3;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(24, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "0";
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Leelawadee UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 30);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "R";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Leelawadee UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(59, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 30);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "G";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Leelawadee UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(90, 4);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 30);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "B";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(60, 33);
+            this.textBox2.MaxLength = 3;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(24, 20);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.Text = "0";
+            this.textBox2.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyUp);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(90, 33);
+            this.textBox3.MaxLength = 3;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(24, 20);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.Text = "0";
+            this.textBox3.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox3_KeyUp);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Leelawadee UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(284, 3);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Preview";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(393, 133);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.DeepPink;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "WinRGB";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WinRGB/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Microsoft.Win32;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1() {
+            InitializeComponent();
+            this.KeyPreview = true;
+        }
+
+        public void RGBRect(int R, int G, int B) {
+            System.Drawing.SolidBrush myBrush = new System.Drawing.SolidBrush(System.Drawing.Color.FromArgb(255, R, G, B));
+            System.Drawing.Graphics formGraphics;
+            formGraphics = this.CreateGraphics();
+            formGraphics.FillRectangle(myBrush, new Rectangle(284, 24, 75, 75));
+            myBrush.Dispose();
+            formGraphics.Dispose();
+        }
+
+        void exportRegistry(string strKey, string filepath)
+        {
+            try
+            {
+                using (Process proc = new Process())
+                {
+                    proc.StartInfo.FileName = "reg.exe";
+                    proc.StartInfo.UseShellExecute = false;
+                    proc.StartInfo.CreateNoWindow = true;
+                    proc.StartInfo.RedirectStandardOutput = true;
+                    proc.StartInfo.RedirectStandardError = true;
+                    proc.StartInfo.Arguments = "export \"" + strKey + "\" \"" + filepath + "\" /y";
+                    proc.Start();
+                    string stdout = proc.StandardOutput.ReadToEnd();
+                    string stderr = proc.StandardError.ReadToEnd();
+                    proc.WaitForExit();
+                    MessageBox.Show(this, String.Format("WinRGB Registry Backup: {0}", stdout), "WinRGB", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, String.Format("Exception: {0}", ex), "WinRGB Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        void writeRegistry() {
+            try
+            {
+                RegistryKey rgbKey = Registry.CurrentUser.OpenSubKey("Control Panel\\Colors", true);
+                if (rgbKey != null)
+                {
+                    rgbKey.SetValue("Hilight", String.Format("{0} {1} {2}", textBox1.Text, textBox2.Text, textBox3.Text), RegistryValueKind.String);
+                    rgbKey.SetValue("HotTrackingColor", String.Format("{0} {1} {2}", textBox1.Text, textBox2.Text, textBox3.Text), RegistryValueKind.String);
+                    rgbKey.Close();
+                    MessageBox.Show(this, "All done! Re-log or restart your computer to apply changes.", "WinRGB", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            } catch (Exception ex)
+            {
+                MessageBox.Show(this, String.Format("Exception: {0}", ex), "WinRGB Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            if (int.TryParse(textBox1.Text, out int res1) && int.TryParse(textBox2.Text, out int res2) && int.TryParse(textBox3.Text, out int res3)) {
+                if ((res1 >= 0 && res1 <= 255) && (res2 >= 0 && res2 <= 255) && (res3 >= 0 && res3 <= 255)) {
+                    var msgBoxResult = MessageBox.Show(this, String.Format("Are you sure you want to use entered values?\nR:{0} G:{1} B:{2}", textBox1.Text, textBox2.Text, textBox3.Text), "WinRGB", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if (msgBoxResult == DialogResult.Yes) {
+                        var cwd = System.AppContext.BaseDirectory;
+                        MessageBox.Show(this, String.Format("Registry will be backed up into:\n{0}reg_backup.reg", cwd), "WinRGB", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        exportRegistry(@"HKEY_CURRENT_USER\Control Panel\Colors", String.Format(@"{0}reg_backup.reg", cwd));
+                        writeRegistry();
+                    }
+                }
+                else {
+                    MessageBox.Show(this, "Entered value is not a valid RGB value! 0-255", "Invalid Value", MessageBoxButtons.OK);
+                }
+            } else {
+                MessageBox.Show(this, "Entered value is not a number!", "Invalid Value", MessageBoxButtons.OK);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+        }
+        public void fixInvalid() {
+            int.TryParse(textBox1.Text, out int R);
+            int.TryParse(textBox2.Text, out int G);
+            int.TryParse(textBox3.Text, out int B);
+            if (R > 255) { textBox1.Text = "255"; } else if (R < 0) { textBox1.Text = "0"; }
+            if (G > 255) { textBox2.Text = "255"; } else if (G < 0) { textBox2.Text = "0"; }
+            if (B > 255) { textBox3.Text = "255"; } else if (B < 0) { textBox3.Text = "0"; }
+        }
+
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e) {
+            if (int.TryParse(textBox1.Text, out int R)) {
+                fixInvalid();
+                if (R >= 0 && R <= 255) {
+                    RGBRect(R, int.Parse(textBox2.Text), int.Parse(textBox3.Text));
+                } else {
+                    fixInvalid();
+                }
+            }
+        }
+
+        private void textBox2_KeyUp(object sender, KeyEventArgs e) {
+            if (int.TryParse(textBox2.Text, out int G)) {
+                fixInvalid();
+                if (G >= 0 && G <= 255) {
+                    RGBRect(int.Parse(textBox1.Text), G, int.Parse(textBox3.Text));
+                } else {
+                    fixInvalid();
+                }
+            }
+        }
+
+        private void textBox3_KeyUp(object sender, KeyEventArgs e) {
+            if (int.TryParse(textBox3.Text, out int B)) {
+                fixInvalid();
+                if (B >= 0 && B <= 255) {
+                    RGBRect(int.Parse(textBox1.Text), int.Parse(textBox2.Text), B);
+                } else {
+                    fixInvalid();
+                }
+            }
+        }
+    }
+}
